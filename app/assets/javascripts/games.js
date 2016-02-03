824f3fa921e8f21b5e85e8a37667c9aaf916b7f7
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2016-02-03 07:27:00 +0100

    placement fixed

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -27,7 +27,7 @@ var tile = function() {
 	n: 8,
 	lastpos: 2,
 	table: [],
-	colors: ["green","red"],
+	colors: ["green","white"],
 	create_table: function(){
 	    for (var i = 0; i < t.n ; i++){
 		t.table[i] = [];
@@ -59,6 +59,18 @@ var tile = function() {
 	    }
 	    return p;
 	},
+	hint: function(){
+	    if (t.state == 0 ){
+		$("#hint").text("Place tile");
+		return;
+	    };
+	    if (t.state == 1 ){
+		$("#hint").text("Rotate tile and press done");
+		return;
+	    };
+
+	    
+	},
 	create_grid_path: function(x,y,size){
 	    d = "M " + x + " " + y + "  " + 
 	    "L "  + x + " " + (y+size) + " " +
@@ -79,30 +91,66 @@ var tile = function() {
 	},
 	check_placement: function(row,col,pos){
 	    a = t.position_penetrating(pos);
+	    console.log(a);
+	    if ( row == 0 && a[0] == 1) {
+		console.log("In first row and penetrating north!");
+		return false;
+	    }
 	    
-	    if ( row == 0 && a[0] == 1) return false;
-	    if ( row == t.n-1 && a[2] == 1) return false;
-	    if ( col == 0 && a[3] == 1) return false;
-	    if ( col == t.n-1 && a[1] == 1) return false;
+	    if ( row == t.n-1 && a[2] == 1) {
+		console.log("In last row and penetrating south!"); 
+		return false;
+	    }
+	    if ( col == 0 && a[3] == 1) {
+		console.log("In first column and penetrating west!");
+		return false;
+	    }
+	    if ( col == t.n-1 && a[1] == 1) {
+		console.log("In last column an penetrating east!");
+		return false;
+	    }
 	    
 	    if ( row != 0 && t.table[row-1][col].what != 0 ){
-		b = t.position_penetrating(t.table[row-1][col].pos);
-		if ( b[2] == 1 || a[0] == 1) return false;
+		north = t.table[row-1][col];
+		if ( north.pos != 6 ) {
+		    b = t.position_penetrating(north.pos);
+		    if ( b[2] == 1 || a[0] == 1 ) {
+			console.log("On my north is tile which is not at 6 position and I penetrating north!");
+			return false;
+		    }
+		}
 	    }
 	    
 	    if (row != t.n-1 && t.table[row+1][col].what != 0 ){
-		b = t.position_penetrating(t.table[row+1][col].pos);
-		if ( b[0] == 1 || a[2] == 1) return false;
+		south = t.table[row+1][col];
+		if (south.pos != 2 ){
+		    b = t.position_penetrating(south.pos);
+		    if ( b[0] == 1 || a[2] == 1) {
+			console.log("On my south is tile which is not at 2 position and I penetrating south!");
+			return false;
+		    }
+		}
 	    }
 
 	    if (col != 0 && t.table[row][col-1].what != 0 ){
-
-		b = t.position_penetrating(t.table[row][col-1].pos);
-		if ( b[1] == 1 || a[3] == 1) return false;
+		west = t.table[row][col-1];
+		if (west.pos != 4 ) {
+		    b = t.position_penetrating(west.pos);
+		    if ( b[1] == 1 || a[3] == 1) {
+			console.log("On my west is tile which is not at 4 position and I penetrating west!");
+			return false;
+		    }
+		}
 	    }
 	    if (col != t.n-1 && t.table[row][col+1].what != 0 ){
-		b = t.position_penetrating(t.table[row][col+1].pos);
-		if ( b[3] == 1 || a[1] == 1) return false;
+		east = t.table[row][col+1];
+		if (east.pos != 0){
+		    b = t.position_penetrating(east.pos);
+		    if ( b[3] == 1 || a[1] == 1) {
+			console.log("On my east is tile which is not at 0 position and I penetrating east!");
+			return false;
+		    }
+		}
 	    }
 	    return true;
 	},
@@ -124,6 +172,12 @@ var tile = function() {
 	    pos.s = s;
 	    return pos;
 	},
+	bind_done: function(){
+	    $("#done").click(function(){
+		t.state = 0;
+		t.done(false);
+	    });
+	},
 	mouse_place: function(e){
 	    var what = 0;
 	    if(t.last_what == 1 ) { what = 2; } else { what = 1; }; 
@@ -135,9 +189,15 @@ var tile = function() {
 	    if (tt.what != 0){
 		return;
 	    }
-	    t.draw_tile(row,col,0,what);
-	    t.state = 1;
-	    t.last_what = what;
+	    for ( var whatpos = 0; whatpos <= 7; whatpos++){
+		if(t.check_placement(row,col,whatpos)){
+		    t.draw_tile(row,col,whatpos,what);
+		    t.state = 1;
+		    t.last_what = what;
+		    t.done(true);
+		    return;
+		}
+	    }
 	    
 	},
 	mouse_down: function(e){
@@ -145,6 +205,15 @@ var tile = function() {
 		t.mouse_rotate(e);
 	    } else {
 		t.mouse_place(e);
+		t.hint();
+	    }
+	},
+	done: function(show){
+	    if (show) {
+		$("#done").show("slow");
+	    } else {
+		$("#done").hide("slow");
+		t.hint();
 	    }
 	},
 	mouse_rotate: function(e){
@@ -184,7 +253,6 @@ var tile = function() {
 		    
 		if (t.lastpos === null) { return; }
 		tt = t.table[t.lastpos.row][t.lastpos.col];
-   		console.log(tt + "ROW " + t.lastpos.row + " " + t.lastpos.col);
 	    });
 	},
 	draw_tile: function(row,col,pos,c){
@@ -221,14 +289,14 @@ var tile = function() {
     return t;
 }();
 
-$( function(){ 	       tile.create_table();
-		       tile.bind_mouse();
-		       tile.draw_grid();
-		       tile.draw_tile(3,4,0,1);
-		       tile.draw_tile(4,4,0,1);
-		       tile.draw_tile(5,4,7,2);
-		       $('#tiles').mousedown(tile.mouse_down);
-			   
-		       
- 				      
-	     } );
+
+$( function(){
+    tile.done(false);
+    tile.create_table();
+    tile.bind_mouse();
+    tile.draw_grid();
+    $('#tiles').mousedown(tile.mouse_down);
+    tile.bind_done();
+    tile.state = 0;
+    tile.hint();
+} );

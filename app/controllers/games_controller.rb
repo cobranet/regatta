0ab5dda5baad2e417a4dd303e5d8c945e59e7ba7
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2016-03-28 08:42:52 +0200

    fixing command buttons

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,15 @@ class GamesController < ApplicationController
   def index
   end
   def save
+    game = Game.find(params[:id].to_i)
+    if game
+      game.game = params[:data]
+    else
+      game = Game.new
+      game.id = params[:id].to_i
+      game.game = params[:data]
+    end
+    game.save!
   end
   def show
   end
